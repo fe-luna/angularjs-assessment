35a60b1f27c8a920aa3b6ad988040bc8b60762e7
fe-luna <feluna.zhang@gmail.com>
2023-08-31 12:23:49 +0800

    feat: click to chat

--- client/app/components/sidebar/sidebar.controller.js
+++ client/app/components/sidebar/sidebar.controller.js
@@ -1,11 +1,15 @@
 class SidebarController {
-  constructor(StateService) {
+  constructor(StateService, $location) {
     this.name = "sidebar";
     this.contacts = StateService.getContacts();
     console.log(this.contacts);
+
+    this.toChat = function (uid) {
+      $location.path(`/chat/${uid}`);
+    };
   }
 }
 
-SidebarController.$inject = ["StateService"];
+SidebarController.$inject = ["StateService", "$location"];
 
 export default SidebarController;
