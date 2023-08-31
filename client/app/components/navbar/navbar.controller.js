66c9bb9d79c8b625f5d0e0a063bae83375d3e997
fe-luna <feluna.zhang@gmail.com>
2023-08-31 16:56:54 +0800

    feat: add tips

--- client/app/components/navbar/navbar.controller.js
+++ client/app/components/navbar/navbar.controller.js
@@ -1,7 +1,14 @@
 class NavbarController {
-  constructor() {
-    this.name = 'navbar';
+  constructor(StateService, $location) {
+    this.name = "navbar";
+    this.currentUser = StateService.getCurrentUser();
+
+    this.toHome = function () {
+      $location.path(`/`);
+    };
   }
 }
+
+NavbarController.$inject = ["StateService", "$location"];
 
 export default NavbarController;
